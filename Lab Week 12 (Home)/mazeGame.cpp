2f52088cef9abfb0231804fd2972bb3a3a8b162a
SadafSyeda-321 <sadafsyeda321@gmail.com>
2026-05-03 23:55:04 +0500

    Game

---
+++ Lab Week 12 (Home)/mazeGame.cpp
@@ -0,0 +1,364 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void printMaze()
+{
+    cout << "###################################################" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "#                                                 #" << endl;
+    cout << "###################################################" << endl;
+}
+
+// Player Character
+char box = 219;
+char tank1[6] = {box, box, box, '-', '-', '>'};
+char tank2[6] = {'0', ' ', '0', ' ', ' ', ' '};
+
+// Enemy Character
+char enemy1[6] = {' ', ' ', ' ', '-', '-', '-'};
+char enemy2[6] = {'<', '=', '=', '(', '-', ')'};
+char enemy3[6] = {' ', ' ', ' ', '\\', '@', '/'};
+char enemy4[6] = {' ', ' ', ' ', '*', '*', '*'};
+
+//  Player Coordinate
+int tankX = 5;
+int tankY = 5;
+
+//  Enemy Coordinates
+int enemyX = 30;
+int enemyY = 10;
+
+int timer = 0;
+int score = 0;
+string enemyDirection = "Up";
+void eraseTank();
+void printEnemy();
+void eraseEnemy();
+void printTank();
+void moveTankLeft();
+void generateBullet();
+void makeBulletActive(int index);
+void makeBulletInactive(int index);
+
+void addScore();
+void eraseBullet();
+void printScore();
+void moveBullet();
+
+// function to be noted
+void gotoxy(int x, int y)
+{
+    COORD coordinates;
+    coordinates.X = x;
+    coordinates.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
+}
+char getCharAtxy(short int x, short int y)
+{
+    CHAR_INFO ci;
+    COORD xy = {0, 0};
+    SMALL_RECT rect = {x, y, x, y};
+    COORD coordBufSize;
+    coordBufSize.X = 1;
+    coordBufSize.Y = 1;
+    return ReadConsoleOutput(GetStdHandle(STD_OUTPUT_HANDLE), &ci, coordBufSize, xy, &rect) ? ci.Char.AsciiChar : ' ';
+}
+
+void printTank()
+{
+    gotoxy(tankX, tankY);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << tank1[index];
+    }
+    gotoxy(tankX, tankY + 1);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << tank2[index];
+    }
+}
+
+void printEnemy()
+{
+    gotoxy(enemyX, enemyY);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << enemy1[index];
+    }
+    gotoxy(enemyX, enemyY + 1);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << enemy2[index];
+    }
+    gotoxy(enemyX, enemyY + 2);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << enemy3[index];
+    }
+    gotoxy(enemyX, enemyY + 3);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << enemy4[index];
+    }
+}
+
+// Player Movement
+void moveTankLeft()
+{
+    char next = getCharAtxy(tankX - 1, tankY);
+    if (next == ' ')
+    {
+        eraseTank();
+        tankX = tankX - 1;
+        printTank();
+    }
+}
+void moveTankRight()
+{
+    char next = getCharAtxy(tankX + 6, tankY);
+    if (next == ' ')
+    {
+        eraseTank();
+        tankX = tankX + 1;
+        printTank();
+    }
+}
+
+void moveTankUp()
+{
+    char next = getCharAtxy(tankX, tankY - 1);
+    if (next == ' ')
+    {
+        eraseTank();
+        tankY = tankY - 1;
+        printTank();
+    }
+}
+void moveTankDown()
+{
+    char next = getCharAtxy(tankX, tankY + 2);
+    if (next == ' ')
+    {
+        eraseTank();
+        tankY = tankY + 1;
+        printTank();
+    }
+}
+
+// Enemy Movement
+void moveEnemy()
+{
+    if (enemyDirection == "Up")
+    {
+        char next = getCharAtxy(enemyX, enemyY - 1);
+        if (next == ' ')
+        {
+            eraseEnemy();
+            enemyY--;
+            printEnemy();
+        }
+        if (next == 'a')
+        {
+            enemyDirection = "Down";
+        }
+    }
+    if (enemyDirection == "Down")
+    {
+        char next = getCharAtxy(enemyX, enemyY + 4);
+        if (next == ' ')
+        {
+            eraseEnemy();
+            enemyY++;
+            printEnemy();
+        }
+        if (next == '#')
+        {
+            enemyDirection = "Up";
+        }
+    }
+}
+
+void eraseEnemy()
+{
+    gotoxy(enemyX, enemyY);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << " ";
+    }
+    gotoxy(enemyX, enemyY + 1);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << " ";
+    }
+    gotoxy(enemyX, enemyY + 2);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << " ";
+    }
+    gotoxy(enemyX, enemyY + 3);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << " ";
+    }
+}
+
+void eraseTank()
+{
+    gotoxy(tankX, tankY);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << " ";
+    }
+    gotoxy(tankX, tankY + 1);
+    for (int index = 0; index < 6; index++)
+    {
+        cout << " ";
+    }
+}
+
+// Global Array & Variables
+// Player Bullets
+int bulletX[100];
+int bulletY[100];
+bool isBulletActive[100];
+int bulletCount = 0;
+
+// Generate Bullet
+void generateBullet()
+{
+    bulletX[bulletCount] = tankX + 7;
+    bulletY[bulletCount] = tankY;
+    isBulletActive[bulletCount] = true;
+    gotoxy(tankX + 7, tankY);
+    cout << ".";
+    bulletCount++;
+}
+// Move Bullet
+void moveBullet()
+{
+    for (int x = 0; x < bulletCount; x++)
+    {
+        if (isBulletActive[x] == true)
+        {
+            char next = getCharAtxy(bulletX[x] + 1, bulletY[x]);
+            if (next != ' ')
+            {
+                eraseBullet(bulletX[x], bulletY[x]);
+                makeBulletActive(x);
+            }
+            else
+            {
+                eraseBullet(bulletX[x], bulletY[x]);
+                bulletX[x] = bulletX[x] + 1;
+                printBullet(bulletX[x], bulletY[x]);
+            }
+        }
+    }
+}
+//  Supporting Functions
+void printBullet(int x, int y)
+{
+    gotoxy(x, y);
+    cout << ".";
+}
+void eraseBullet(int x, int y)
+{
+    gotoxy(x, y);
+    cout << " ";
+}
+void makeBulletActive(int index)
+{
+    isBulletActive[index] = false;
+}
+void makeBulletInactive(int index)
+{
+    isBulletActive[index] = false;
+}
+
+// Collision with Enemy
+void bulletCollisionWithEnemy()
+{
+    for (int x = 0; x < bulletCount; x++)
+    {
+        if (isBulletActive[x] == true)
+        {
+            if (bulletX[x] + 1 == enemyX && (bulletY[x] == enemyY || bulletY[x] == enemyY + 2 || bulletY[x] == enemyY + 3))
+            {
+                addScore();
+            }
+            if (enemyX - 1 == bulletX[x] && enemyY + 1 == bulletY[x])
+            {
+                addScore();
+            }
+        }
+    }
+}
+// Score
+void addScore()
+{
+    score++;
+}
+
+void printScore()
+{
+    gotoxy(45, 8);
+    cout << "Score: " << score;
+}
+
+// Main Functions
+main()
+{
+    system("cls");
+    printMaze();
+    printTank();
+    printEnemy();
+    while (true)
+    {
+        printScore();
+        if (GetAsyncKeyState(VK_LEFT))
+        {
+            moveTankLeft();
+        }
+        if (GetAsyncKeyState(VK_RIGHT))
+        {
+            moveTankRight();
+        }
+        if (GetAsyncKeyState(VK_UP))
+        {
+            moveTankUp();
+        }
+        if (GetAsyncKeyState(VK_DOWN))
+        {
+            moveTankDown();
+        }
+        if (GetAsyncKeyState(VK_SPACE))
+        {
+            generateBullet();
+        }
+        if (timer == 3)
+        {
+            moveEnemy();
+            timer = 0;
+        }
+        moveBullet();
+        bulletCollisionWithEnemy();
+        timer++;
+        Sleep(90);
+    }
+}
